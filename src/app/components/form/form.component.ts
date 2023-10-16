ca8e9fdb562caf586cdfb6cd17110b32a7c380eb
AnsBenO <ans.benomar@gmail.com>
2023-10-16 20:59:47 +0100

    fix space problem in input

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -21,7 +21,7 @@ export class FormComponent implements OnInit {
                 [
                     Validators.required,
                     Validators.minLength(4),
-                    Validators.pattern(/^[a-zA-Z0-9_-]+$/),
+                    Validators.pattern(/^[a-zA-Z0-9_-]+[\s]+$/),
                 ],
             ],
             email: ["", [Validators.required, Validators.email]],
